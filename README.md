56773003bf7fa71887404d00e933e3f032d8465a
Unknown <asdf0rrr@gmail.com>
2017-03-08 17:52:31 +0100

    Modified files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is a test repository folder!
+
+Try to pull and play with it.
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+// always add comments in your code
 #include <stdio.h>
-
+// that's a hello world application
 int main(void) {
-
+	
 	printf("Hello World\n");
 	
 	return 0;
